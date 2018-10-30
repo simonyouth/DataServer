9034e27ddfba6aa56de25546dc3ef075a24e0d56
simonyouth <2515240522@qq.com>
2018-10-30 17:41:38 +0800

    add line chart data

--- store.js
+++ store.js
@@ -1,109 +1,12 @@
-const randomInt = require('./utils');
-
-function getScatterData() {
-	const arr = [];
-	for (let i = 0; i < 50; i++) {
-		arr[i] = {
-			x: Math.random() * 50,
-			y: Math.random() * 100,
-		};
-	}
-	return arr;
-}
-
-function getPieData() {
-	const arr = [];
-	const count = randomInt(2, 7);
-	for (let i = 0; i < count; i++) {
-		arr[i] = {
-			x: `depart${i}`,
-			y: randomInt(1, 400),
-			label: `depart${i}`,
-		};
-	}
-	return arr;
-}
+const data = require('./utils');
 
 module.exports = {
 	response: {
 		httpCode: 200,
 		data: {
-			home: [
-				{
-					"month": 1,
-					"compared": "2.3%",
-					"lastYear": 1004,
-					"thisYear": 1300,
-				},
-				{
-					"month": 2,
-					"compared": "10%",
-					"lastYear": 2342,
-					"thisYear": 3345,
-				},
-				{
-					"month": 3,
-					"compared": "-12%",
-					"lastYear": 4450,
-					"thisYear": 2345,
-				},
-				{
-					"month": 4,
-					"compared": "9%",
-					"lastYear": 900,
-					"thisYear": 990,
-				},
-				{
-					"month": 5,
-					"compared": "133%",
-					"lastYear": 1004,
-					"thisYear": 4655,
-				},
-				{
-					"month": 6,
-					"compared": "-64%",
-					"lastYear": 5643,
-					"thisYear": 1300,
-				},
-				{
-					"month": 7,
-					"compared": "2.3%",
-					"lastYear": 1004,
-					"thisYear": 1300,
-				},
-				{
-					"month": 8,
-					"compared": "7%",
-					"lastYear": 7003,
-					"thisYear": 7790,
-				},
-				{
-					"month": 9,
-					"compared": "6%",
-					"lastYear": 3453,
-					"thisYear": 3676,
-				},
-				{
-					"month": 10,
-					"compared": "-9%",
-					"lastYear": 3000,
-					"thisYear": 2360,
-				},
-				{
-					"month": 11,
-					"compared": "3%",
-					"lastYear": 5430,
-					"thisYear": 5900,
-				},
-				{
-					"month": 12,
-					"compared": "13%",
-					"lastYear": 4000,
-					"thisYear": 5300,
-				},
-			],
-			patient: getScatterData(),
-			depart: getPieData(),
+			line: data.getLineData(),
+			patient: data.getScatterData(),
+			depart: data.getPieData(),
 		}
 	},
 };
--- utils.js
+++ utils.js
@@ -1,5 +1,47 @@
-function randomInt(min, max) {
+function int(min, max) {
 	return Math.floor(Math.random() * (max - min));
 }
 
-module.exports = randomInt;
+function num(min, max) {
+	return (Math.random() * (max - min));
+}
+
+function getScatterData() {
+	const arr = [];
+	for (let i = 0; i < 50; i++) {
+		arr[i] = {
+			x: Math.random() * 50,
+			y: Math.random() * 100,
+		};
+	}
+	return arr;
+}
+
+function getPieData() {
+	const arr = [];
+	for (let i = 0; i < 5; i++) {
+		arr[i] = {
+			x: `depart${i}`,
+			y: int(1, 400),
+			label: `depart${i}`,
+		};
+	}
+	return arr;
+}
+
+function getLineData() {
+	const arr = [];
+	for (let i = 0; i < 6; i++) {
+		arr[i] = {
+			x: i,
+			y: int(0, 1000)
+		}
+	}
+	return arr;
+}
+
+module.exports = {
+	getPieData,
+	getScatterData,
+	getLineData,
+};
